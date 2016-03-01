2b8383e53cfd5865abb616c45cdecf322a98fb12
GusDeCooL <gusdecool@gmail.com>
2016-03-01 16:17:27 +0800

    release 1.0.2

--- src/angular-wrapper.js
+++ src/angular-wrapper.js
@@ -1,18 +1,13 @@
 /**
  * Angular Wrapper
  * @author Budi Arsana <gusdecool@gmail.com>
- * @version 1.0.0
  */
-'use strict';
+(function() {
+    'use strict';
 
-angular.module('angularWrapper', []).factory('jQuery', function() {
-    return window.jQuery;
-});
-
-angular.module('angularWrapper', []).factory('_', function() {
-    return window._;
-});
-
-angular.module('angularWrapper', []).factory('moment', function() {
-    return window.moment;
-});
+    angular.module('angularWrapper', [])
+        .factory('jQuery', function() { return window.jQuery; })
+        .factory('_', function() { return window._; })
+        .factory('moment', function() { return window.moment; })
+    ;
+})();
